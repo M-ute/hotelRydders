c257d86a9647b490fde3366513683f1d0880e2ab
M-ute <wiaboamos76@gmail.com>
2026-06-03 03:21:33 +0000

    Menu and Close button states

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ const Header = () => {
         <div className="container text-center mx-auto px-6 py-6 text-white md:px-20 lg:px-32">
             <h2 className="text-5xl sm:text-6xl md:text-[80px] inline-block max-w-3xl font-semibold py-20">Explore Your Homes That Fits Your Dreams</h2>
             <div className="space-x-6 mt-16 text-lg">
-                <a href="" className="border-3 rounded-4xl border-white px-8 py-4 text-green-400 hover:bg-amber-50">Projects</a>
-                <a href="" className="bg-white rounded-4xl px-8 py-4 text-amber-500 hover:bg-amber-50">Contacts Us</a>
+                <a href="" className="border-3 rounded-4xl border-white px-8 py-4 text-white hover:bg-amber-50 hover:text-black">Projects</a>
+                <a href="" className="bg-white rounded-4xl px-8 py-4 text-black hover:bg-gray-700 hover:text-white">Contact Us</a>
             </div>
         </div>
         
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import {assets} from "../assets/assets"
 
 const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <div className="absolute top-0 left-0 w-full z-0">
-        <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32
+        <div className="container mx-auto flex justify-between items-center py-4 px-4 md:px-10 lg:px-32
         bg-transparent rounded-lg">
             <img src={assets.logo} alt="" />
                 <ul className="hidden md:flex gap-7 text-white" >
@@ -13,8 +15,27 @@ const Navbar = () => {
                     <a href="#Header" className="cursor-pointer hover:text-gray-500">Contact</a>
                     <a href="#Header" className="cursor-pointer hover:text-gray-500">Testimonials</a>
                 </ul>
-            <button className="hidden text-white md:block bg-amber-5co00 px-8 py-2 rounded-full">Sign Up</button>
+            <button className="hidden text-white md:block bg-amber-500
+             hover:bg-white hover:text-black px-8 py-2 rounded-full">Sign Up
+            </button>
+            <img onClick={() => setShowMobileMenu(true)} src={assets.menu_icon} alt="" className="md:hidden w-7 cursor-pointer " />
+        </div>
+        {/*-----------MOBILE MENU---------*/}
+        <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'hidden'} top-0 right-0 bottom-0 overflow-hidden
+          transition-all`}>
+            <div className="flex justify-end cursor-pointer px-3 py-5 bg-white ">
+                <img onClick={() => setShowMobileMenu(false)} src={assets.cross_icon} alt="" className="w-8 p-1 border-2 border-red-600 rounded-full " />
+            </div>
+            <ul className="flex flex-col gap-4 items-center  text-gray-800 text-center text-lg 
+            font-medium px-5 py-1 bg-white ">
 
+                <a href="#Header" className=" px-4 py-2 rounded-full inline-block">Home</a>
+                <a href="#Header" className=" px-4 py-2 rounded-full inline-block">About</a>
+                <a href="#Header" className=" px-4 py-2 rounded-full inline-block">Services</a>
+                <a href="#Header" className=" px-4 py-2 rounded-full inline-block">Contact</a>
+                <a href="#Header" className=" px-4 py-2 rounded-full inline-block">Testimonials</a>
+
+            </ul>
         </div>
 
     </div>
