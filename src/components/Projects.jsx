a73e802ac6875a05fd5032249a750fe8925c4aff
M-ute <wiaboamos76@gmail.com>
2026-06-05 23:16:29 +0000

    next and previous buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,4 @@
+import { assets } from "../assets/assets"
 
 const Projects = () => {
   return (
@@ -9,6 +10,21 @@ const Projects = () => {
             Crafting Spaces, Building Legacies - Explore Our Portfolio </p>
 
             {/*-------SLIDER BUTTONS-------*/}
+            <div className="flex items-center justify-end mb-8">
+              <button className="p-3 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 
+              px-4 rounded-full mr-2" ariel-label="Previous Project">
+                <img src={assets.left_arrow} alt="Previous" />
+              </button>
+              <button className="p-3 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 
+              px-4 rounded-full mr-2" ariel-label="Next Project">
+                <img src={assets.right_arrow} alt="Next" />
+              </button>
+            </div>
+
+            {/*-------PROJECTS SLIDER CONTAINER-------*/}
+            
+
+
     </div>
   )
 }
